74d32cc23e50792d41590297b24ac922c3b6eddb
Bing Gan <co2.gerlaic@gmail.com>
2021-03-30 22:33:53 -0700

    react-draft-wysiwyg init puish

--- app/components/VPEditor.jsx
+++ app/components/VPEditor.jsx
@@ -1,44 +1,26 @@
-import { Editor, EditorState, RichUtils } from 'draft-js';
-import React from 'react';
+import React, { Component } from "react";
+import { Editor } from "react-draft-wysiwyg";
+// import "../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+// import "../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css"
 
 export default class VPEditor extends React.Component {
+  render() {
+    return (
+      <div className="VPEditor-root">
+        <div className="ui-input-text-area article-title" typeof="text">
+          <textarea
+            placeholder="请输入标题（建议30字以内）"
+            maxLength="60"
+            rows="1"
+            style={{ overflowY: "hidden" }}
+          ></textarea>
+          <span className="count">0/40</span>
+        </div>
 
-
-    constructor(props) {
-        super(props);
-        this.state = { editorState: EditorState.createEmpty() };
-        this.onChange = (editorState) => this.setState({ editorState });
-    }
-
-    _onBoldClick() {
-        this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, `BOLD`));
-    }
-
-    render() {
-        const {editorState} = this.state;
-
-        let className = "VPEditor";
-        var contentState = editorState.getCurrentContent();
-        if (!contentState.hasText()) {
-            if (contentState.getBlockMap().first().getType() !== "unstyled") {
-                className += ' VPEditor-hidPlaceholder';
-            }
-        }
-
-        return (
-            <div className="VPEditor-root">
-                <div className="ui-input-text-area article-title" typeof="text">
-                    <textarea placeholder="请输入标题（建议30字以内）" maxLength="60" rows="1" style={{ overflowY: "hidden" }}></textarea>
-                    <span className="count">0/40</span>
-                </div>
-                
-                <div>
-                    <button onClick={this._onBoldClick.bind(this)}>Bold</button>
-                </div>
-                <div className="editor">
-                    <Editor editorState={this.state.editorState} onChange={this.onChange} />
-                </div>
-            </div>
-        );
-    }
+        <div className="editor">
+          <Editor />
+        </div>
+      </div>
+    );
+  }
 }
